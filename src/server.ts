16c330155c89d7a4247c90306fb305a7725acb20
leonardo-policarpo <leo.dumont@outlook.com>
2025-07-10 00:15:46 -0300

    feat: add room list

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
+import { getRoomRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -19,5 +20,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.get('/health', () => {
   return 'OK';
 });
+
+app.register(getRoomRoute);
 
 app.listen({ port: env.PORT });
